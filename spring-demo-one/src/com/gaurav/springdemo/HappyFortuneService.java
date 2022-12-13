2b1d52f8bd73872af3e7dac902f1539ed5ee4ff8
Gaurav Sahal <gaurav.sahal10@gmail.com>
2022-12-13 13:39:41 +0530

    Added HappyFortuneService.java

---
+++ spring-demo-one/src/com/gaurav/springdemo/HappyFortuneService.java
@@ -0,0 +1,10 @@
+package com.gaurav.springdemo;
+
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return "Today is your lucky day";
+	}
+
+}
