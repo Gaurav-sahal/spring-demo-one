a94c9022c3f78e98e47b6de2d02fc78d0d53cc39
Gaurav Sahal <gaurav.sahal10@gmail.com>
2022-12-16 22:27:43 +0530

    Added TrackCoach.java

---
+++ spring-demo-one/src/com/gaurav/springdemo/TrackCoach.java
@@ -0,0 +1,35 @@
+package com.gaurav.springdemo;
+
+public class TrackCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+	}
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just Do It: " + fortuneService.getFortune();
+	}
+	
+	
+	//add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	//add a destroy method
+	public void doMyCleanStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanStuffYoYo");
+	}
+
+}
