896666c1dbd6e77457a943fbef1237ca6309bfdc
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-02-07 22:52:43 +0530

    Added Spring-demo-aop-pointcut-declarations-combo

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/gaurav/aopdemo/MainDemoApp.java
@@ -0,0 +1,43 @@
+package com.gaurav.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.gaurav.aopdemo.dao.AccountDAO;
+import com.gaurav.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+		//read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		//get membership bean from spring container
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+		
+		//call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		//call the accountdao getter/setter methods
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+		
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+		
+		//call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+
+		//close the context
+		context.close();
+	}
+
+}
