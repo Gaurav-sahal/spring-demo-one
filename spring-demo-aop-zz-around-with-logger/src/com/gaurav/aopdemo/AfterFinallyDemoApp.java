aac30c17d667ebd72c94f898aff16981b6a48d50
Gaurav Sahal <gaurav.sahal10@gmail.com>
2023-02-28 23:14:21 +0530

    Added AfterFinallyDemoApp

---
+++ spring-demo-aop-zz-around-with-logger/src/com/gaurav/aopdemo/AfterFinallyDemoApp.java
@@ -0,0 +1,42 @@
+package com.gaurav.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.gaurav.aopdemo.dao.AccountDAO;
+
+public class AfterFinallyDemoApp {
+
+	public static void main(String[] args) {
+
+		//read spring config java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		//call method to find the accounts
+		List<Account> theAccounts = null;
+		
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = false;
+			theAccounts = theAccountDAO.findAccounts(tripWire);			
+		}
+		catch (Exception exc) {
+			System.out.println("\n\nMain Program ... caught exception: "+ exc);
+		}
+		
+		//display the accounts
+		System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+		System.out.println("-------");
+		
+		System.out.println(theAccounts);
+
+		//close the context
+		context.close();
+	}
+
+}
